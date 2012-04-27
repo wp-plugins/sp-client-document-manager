9c115a28a3fbd1f2d4ec6dbef16e31e282167a51
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-27 19:43:44 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.0.8@537471 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions.php
@@ -0,0 +1,385 @@
+<?php
+
+
+
+if (!function_exists('sp_client_upload_settings')){
+
+function sp_client_upload_settings(){
+	
+global $wpdb;
+	
+	
+	if($_GET['save_options'] == 1){
+		
+
+		
+			foreach( $_POST as $key => $value){
+				
+			
+	
+				
+				update_option( $key,$value ); 
+	
+			}
+			
+			
+				
+	if($_POST['sp_cu_user_projects'] == "1"){update_option('sp_cu_user_projects','1' ); }else{update_option('sp_cu_user_projects','0' );	}
+	if($_POST['sp_cu_js_redirect'] == "1"){update_option('sp_cu_js_redirect','1' ); }else{update_option('sp_cu_js_redirect','0' );	}		
+			
+	}
+	
+	
+	
+	if(get_option('sp_cu_user_projects') == 1){ $sp_cu_user_projects = ' checked="checked" ';	}else{ $sp_cu_user_projects = '  '; }
+	if(get_option('sp_cu_js_redirect') == 1){ $sp_cu_js_redirect = ' checked="checked" ';	}else{ $sp_cu_js_redirect = '  '; }
+	echo '<h2>Settings</h2>'.sp_client_upload_nav_menu().'';	 
+	
+	
+	if($_POST['upgrade'] != ""){
+	
+	
+	$mydir = ''.ABSPATH.'wp-content/plugins/sp-client-document-manager/premium/'; 
+	if (is_dir($mydir)){
+	$d = dir($mydir); 
+	while($entry = $d->read()) { 
+	 if ($entry!= "." && $entry!= "..") { 
+	 @unlink($entry); 
+	 } 
+	} 
+	$d->close(); 
+	@rmdir($mydir); 
+	}
+	function _return_direct() { return 'direct'; }
+add_filter('filesystem_method', '_return_direct');
+WP_Filesystem();
+remove_filter('filesystem_method', '_return_direct');
+	
+	global $wp_filesystem;	
+	echo  unzip_file( $_FILES['premium']['tmp_name'],''.ABSPATH.'wp-content/plugins/sp-client-document-manager/' );	
+	echo '<script type="text/javascript">
+<!--
+window.location = "admin.php?page=sp-client-document-manager-settings&cdm-upgrade=1"
+//-->
+</script>';
+	
+	
+	}
+	
+	$content .='
+<div style="border:1px solid #CCC;padding:5px;margin:5px;background-color:#e3f1d4;">';
+
+if(CU_PREMIUM != 1){
+
+$content .='<h3>Upgrade to premium!</h3>
+<p>If you would like to see the extra features and upgrade to premium please purchase the addon package by <a href="http://smartypantsplugins.com/sp-client-document-manager/" target="_blank">clicking here</a>. Once purchased you will receive a file, upload that file here and the plugin will do the rest!</p>';
+}else{
+$content .='<h3>Thanks for upgrading!</h3>
+<p>You can patch the premium version with the upload form below once new versions become available!</p>';
+}
+	$content .='
+<form action="" method="post" enctype="multipart/form-data">
+<input type="file" name="premium"> <input type="submit" name="upgrade" value="Install Premium!">
+</form>
+
+</div>
+<h2>Settings</h2>
+
+	<form action="admin.php?page=sp-client-document-manager-settings&save_options=1" method="post">
+	 <table class="wp-list-table widefat fixed posts" cellspacing="0">
+  
+   
+         <tr>
+    <td width="300"><strong>Filename Format</strong><br><em>Use the below codes to determine the file format, whatever you put in the box will show up before the actual file name.If you keep this blank then you leave the risk to existing files. Please see the example to the right.</em><br><br>
+	%y =  Year: yyyy<br> 
+	%d =  Day:  dd<br>
+	%m =  Month: mm<br>
+	%h =  Hour: 24 hour format<br>
+	%min = Minute<br>
+	%u = Username<br>
+	%uid = User ID<br>
+	%t = Timstamp<br>
+	%r = Random #<br>
+	
+	</td>
+    <td><input type="text" name="sp_cu_filename_format"  value="'.get_option('sp_cu_filename_format').'"  size=80"><br><div style="margin:5px;padding:5px;"> Example:<br><br>
+	If the user uploads a file called example.pdf and you put<strong>  %y-%m-%d-</strong> the final file name  will be: <strong>'.date("Y").'-'.date("m").'-'.date("d").'-example.pdf</strong></div></td>
+  </tr>
+      <tr>
+    <td width="300"><strong>Thank you message</strong><br><em>This is the thank you text the user sees after they upload.</em></td>
+    <td><input type="text" name="sp_cu_thankyou"  value="'.get_option('sp_cu_thankyou').'"  size=80"> </td>
+  </tr>
+       <tr>
+    <td width="300"><strong>Delete Message</strong><br><em>The confirmation screen asking the user if they want to delete the file.</em></td>
+    <td><input type="text" name="sp_cu_delete"  value="'.get_option('sp_cu_delete').'"  size=80"> </td>
+  </tr>
+  <tr>
+    <td width="300"><strong>WP Folder</strong><br><em>Use this option only if your wp installation is in a sub folder of your url. For instance if your site is www.example.com/blog/ then put /blog/ in the field. This helps find the uploads directory.</em></td>
+    <td><input type="text" name="sp_cu_wp_folder"  value="'.get_option('sp_cu_wp_folder').'"  size=80"> </td>
+  </tr>
+  
+     <tr>
+    <td width="300"><strong>Javascript Redirect?</strong><br><em>If your on a windows system you need to use javascript redirection as FastCGI does not allow force download files.</em></td>
+    <td><input type="checkbox" name="sp_cu_js_redirect"   value="1" '. $sp_cu_js_redirect.'> </td>
+  </tr>
+  ';
+  
+  if (CU_PREMIUM == 1){
+	  $content .='
+     <tr>
+    <td width="300"><strong>User Projects?</strong><br><em>If you want to allow the user to create projects check this box.</em></td>
+    <td><input type="checkbox" name="sp_cu_user_projects"   value="1" '. $sp_cu_user_projects.'> </td>
+  </tr>
+  <tr>
+    <td width="300"><strong>Categories Text</strong><br><em>This is the text you want to call categories, for example you may want to use it as a status.</em></td>
+    <td><input type="text" name="sp_cu_cat_text"  value="'.get_option('sp_cu_cat_text').'"  size=80"> </td>
+  </tr>
+   <tr>
+    <td width="300"><strong>File Deletion Period</strong><br><em>How many days should a file exist before its deleted from the system? Leave blank if you do not wish to use this function.</em></td>
+    <td><input type="text" name="sp_cu_file_delete"  value="'.get_option('sp_cu_file_delete').'"  size=80"> </td>
+  </tr>
+  
+  ';
+  
+  }
+  $content .='
+    <tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="save_options" value="Save Options"></td>
+  </tr>
+</table>
+</form>
+	
+	';
+	
+	
+	
+	echo $content;
+	
+}
+}
+if (!function_exists('sp_client_upload_help')){
+function sp_client_upload_help(){
+	
+echo '<h2>Smarty Pants Client Document Manager</h2>'.sp_client_upload_nav_menu().'
+	
+<p>Please update the page where your uploader shortcode will be placed</p>
+<p>On the page place the shortcode [sp-client-document-manager] to show the uploader</p>
+<p>This plugin relies on 2 other plugins to make a seamless experience, you will want to download install "Theme my login" and "Cemys extra fields" You optionally download "Ajax login" for a nice login page on your sidebar</p>
+<p>Please donate to keep development going on this plugin! <a href="http://smartypantsplugins.com/donate/" target="_blank">http://smartypantsplugins.com/donate/</a></p>
+ 
+';	
+	
+}
+}
+if (!function_exists('sp_client_upload_nav_menu')){
+function sp_client_upload_nav_menu(){
+	global $cu_ver,$sp_client_upload,$sp_cdm_ver ;
+	
+	if(CU_PREMIUM == 1){
+		
+		$ver = $sp_cdm_ver;
+		
+	}else{
+	$ver = $sp_client_upload;	
+	}
+$content .= '<strong>Version:</strong> '.$cu_ver .' '.$ver.'<div style="padding:10px;font-weight:bold">
+
+<a href="admin.php?page=sp-client-document-manager" class="button" style="margin-right:10px">'.__("Home","sp-cdm").'</a>
+<a href="admin.php?page=sp-client-document-manager-settings" class="button" style="margin-right:10px">'.__("Settings","sp-cdm").'</a>
+<a href="admin.php?page=sp-client-document-manager-vendors" class="button" style="margin-right:10px">'.__("Vendors","sp-cdm").'</a>';
+
+if (CU_PREMIUM == 1){
+$content .= '<a href="admin.php?page=sp-client-document-manager-projects" class="button" style="margin-right:10px">'.__("Projects","sp-cdm").'</a>';
+$content .= '<a href="admin.php?page=sp-client-document-manager-categories" class="button" style="margin-right:10px">'.__("Categories","sp-cdm").'</a>';
+}
+$content .= '<a href="admin.php?page=sp-client-document-manager-help" class="button" style="margin-right:10px">'.__("Instructions","sp-cdm").'</a>
+<a href="users.php" class="button"  style="margin-right:10px">'.__("View users","sp-cdm").'</a>
+</div>';	
+	return $content;
+}
+
+}
+if (!function_exists('sp_client_upload_admin')){
+
+function sp_client_upload_admin(){
+	
+	global $wpdb;
+	
+	$user_id = $_REQUEST['user_id'];
+	
+
+if($_GET['dlg-delete-file'] != ""){
+	
+		$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where  id = ".$_GET['dlg-delete-file']."", ARRAY_A);
+		
+		
+		unlink('../wp-content/uploads/sp-client-document-manager/'.$user_id.'/'.$r[0]['file'].'');
+	
+		$wpdb->query("
+	DELETE FROM ".$wpdb->prefix."sp_cu WHERE id = ".$_GET['dlg-delete-file']."
+	");
+	
+
+}
+
+
+
+
+
+	
+	
+	if($user_id != ""){
+		echo '<h2>'.__("User Uploads","sp-cdm").'</h2><a name="downloads"></a>';
+	$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where uid = $user_id  and parent = 0 order by date desc", ARRAY_A);
+	$delete_page = 'user-edit.php?user_id='.$user_id.'';
+	
+	$download_user = '<a href="../wp-content/plugins/sp-client-document-manager/ajax.php?function=download-archive&id='.$user_id.'" class="button">'.__("Click to download all files","sp-cdm").'</a>';
+	}else{
+	$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where  parent = 0 order by id desc LIMIT 150", ARRAY_A);	
+	$html .='<form id="your-profile">';	
+	$delete_page = 'admin.php?page=sp-client-document-manager';
+		$download_user = '';
+	}
+
+if($r == FALSE){
+	
+	
+$html .=  '<p style="color:red">'.__("No Uploads Exist!","sp-cdm").'</p>';
+	
+}else{
+
+
+
+//show uploaded documents
+  $html .= '
+<script type="text/javascript">
+
+function sp_client_upload_email_vendor(){
+	
+
+    	jQuery.ajax({
+			 
+		  type: "POST",
+		  url:  "../wp-content/plugins/sp-client-document-manager/ajax.php?function=email-vendor" ,
+		 
+		 data:  jQuery("#your-profile" ).serialize(),
+		  success: function(msg){
+   								jQuery("#updateme").empty();
+								jQuery("#updateme").append( msg);
+								
+							  }
+ 		});	
+	
+	return false;
+}
+
+
+</script>
+'.	$download_user.'
+  <table class="wp-list-table widefat fixed posts" cellspacing="0">
+	<thead>
+	<tr>
+<th>'.__("File Name","sp-cdm").'</th>
+<th>'.__("User","sp-cdm").'</th>
+<th>'.__("Date","sp-cdm").'</th>
+<th>'.__("Download","sp-cdm").'</th>
+<th>'.__("Email","sp-cdm").'</th>
+</tr>
+	</thead>
+
+
+';
+
+
+
+				for($i=0; $i<count($r); $i++){
+					
+					
+					if($r[$i]['name'] == ""){
+						
+						$name = $r[$i]['file'];
+					}else{
+						
+						$name = $r[$i]['name'];
+			
+		
+		
+					}
+					
+			$r_user = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."users where ID = ".$r[$i]['uid']."", ARRAY_A);				
+				if(get_option('sp_cu_js_redirect') == 1){
+				$target = 'target="_blank"';	
+				}else{
+				$target = ' ';	
+				}	
+					
+					
+				$html .= '
+	
+ <tr>
+    <td ><strong>'.stripslashes($name).'</strong>
+	<br><em>'.__("Notes: ","sp-cdm").' '.$r[$i]['notes'].'</em>
+	
+	</td>
+	<td><a href="user-edit.php?user_id='.$r[$i]['uid'].'">'.$r_user[0]['display_name'].'</a></td>
+	 <td >'.date('F jS Y h:i A', strtotime($r[$i]['date'])).'</td>
+   
+    <td><a style="margin-right:15px" href="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/download.php?fid='.$r[$i]['id'].'" target="_blank">'.__("Download","sp-cdm").'</a> <a href="'.$delete_page .'&dlg-delete-file='.$r[$i]['id'].'&user_id='.$r[$i]['uid'].'#downloads">'.__("Delete","sp-cdm").'</a> </td>
+<td><input type="checkbox" name="vendor_email[]" value="'.$r[$i]['id'].'"></td>	</tr>
+
+
+  
+  ';	
+					
+				}
+			$html .= '</table>
+			
+				<div style="text-align:right">
+			<div id="updateme"></div>
+				'.__("Choose  the files you want to send above, type a message and choose a vendor then click submit:","sp-cdm").'  <select name="vendor">
+				';
+				
+				
+				
+				if($_POST['submit-vendor'] != ""){
+					
+				//	print_r($_POST);
+					
+					
+				}
+				
+			$vendors = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."options   where option_name  LIKE 'sp_client_upload_vendors%'  order by option_name", ARRAY_A);
+
+						for($i=0; $i<count(	$vendors); $i++){
+				
+				$vendor_info[$i] = unserialize($vendors[$i]['option_value']);
+				
+				$html .=  '<option value="'.$vendor_info[$i]['email'].'">'.$vendor_info[$i]['name'].'</option>';
+				
+						}
+				
+				
+				$html .= '</select> '.__("Message:","sp-cdm").' <input type="text" name="vendor-message"> <select name="vendor_attach"><option value="1">'.__("Attach to email:","sp-cdm").' </option><option value="0">'.__("Send links to files","sp-cdm").' </option><option value="3">'.__("Attach and link to to files","sp-cdm").' </option></select> <input type="submit" name="submit-vendor" value="'.__("Email vendor files!","sp-cdm").'" onclick="sp_client_upload_email_vendor();return false;"> 
+				</div>
+				';
+				
+		
+}
+if($user_id != ""){
+echo $html;
+}else{
+	$html .='</form>';	
+return $html;	
+}
+	
+}
+
+
+
+}
+
+
+
+add_action( 'edit_user_profile', 'sp_client_upload_admin' );
+?>
---
+++ shortcode.php
@@ -0,0 +1,395 @@
+<?php
+
+
+function display_sp_thumbnails($r ){
+	global $wpdb,$current_user,$user_ID;
+	
+	
+	
+	if(get_option('sp_cu_wp_folder') == ''){
+	$wp_con_folder = '/';	
+	}else{
+		$wp_con_folder = get_option('sp_cu_wp_folder') ;
+	}
+	$html .= '<script type="text/javascript" src="'. get_bloginfo('url').'/wp-content/plugins/sp-client-document-manager/js/jqueryFileTree/jqueryFileTree.js"></script>
+			<link rel="stylesheet" type="text/css" media="all" href="'. get_bloginfo('url').'/wp-content/plugins/sp-client-document-manager/js/jqueryFileTree/jqueryFileTree.css" />
+		';
+	$html .="
+	<div class=\"sp_cu_filetree\">
+	<script type=\"text/javascript\">
+	
+	jQuery(document).ready( function() {
+    jQuery('#file_manager').fileTree({
+        root: '". $wp_con_folder."wp-content/uploads/sp-client-document-manager/".$user_ID."/',
+        script: '".$wp_con_folder."wp-content/plugins/sp-client-document-manager/ajax.php?function=file-tree&uid=".$user_ID."',
+        expandSpeed: 1000,
+        collapseSpeed: 1000,
+        multiFolder: false
+    }, function(file) {
+       // alert(file);
+	   
+	   
+	   	  var url = '". $wp_con_folder."wp-content/plugins/sp-client-document-manager/ajax.php?function=view-file&id=' + file;
+		 //alert( url);
+            // show a spinner or something via css
+            var dialog = jQuery('<div style=\"display:none\" class=\"loading\"></div>').appendTo('body');
+            // open the dialog
+            dialog.dialog({
+                // add a close listener to prevent adding multiple divs to the document
+                close: function(event, ui) {
+                    // remove div with all data and events
+                    dialog.remove();
+                },
+                modal: true,
+				height:450,
+				width:850
+            });
+            // load remote content
+            dialog.load(
+                url, 
+                {}, // omit this param object to issue a GET request instead a POST request, otherwise you may provide post parameters within the object
+                function (responseText, textStatus, XMLHttpRequest) {
+                    // remove the loading class
+                    dialog.removeClass('loading');
+                }
+            );
+	   
+	   
+	   
+	   
+	   //end functions
+	   
+    });
+});
+	</script>
+	
+	<div id=\"file_manager\">
+	
+	</div></div>
+	";
+
+				
+	
+	return 	$html ;
+	
+}
+
+
+
+
+
+
+function display_sp_thumbnails2($r ){
+	global $wpdb,$current_user,$user_ID;
+	
+	
+		for($i=0; $i<count($r); $i++){
+				$ext = substr(strrchr($r[$i]['file'], '.'), 1);	
+					
+					if($r[$i]['pid'] != 0){
+						  $projecter = $wpdb->get_results("SELECT *
+	
+									 FROM ".$wpdb->prefix."sp_cu_project
+									 WHERE id = '".$r[$i]['pid']."'
+									 ", ARRAY_A);
+									 
+									 
+							$project_title ='Project: '.stripslashes($projecter[0]['name']).'';		 	
+					}else{
+						$project_title = 'Project: None';
+					}
+					
+					
+					if($ext== 'png' or $ext == 'jpg' or $ext = 'jpeg' or $ext = 'gif' ){
+					$icon = '<td><img src="'. get_bloginfo('url').'/wp-content/uploads/sp-client-document-manager/'.$user_ID.'/'.$r[$i]['file'].'" width="90"></td>';	
+					}else{
+					$icon = '';		
+					}
+					
+				$html .= '
+				<div class="sp_cu_item">
+				 
+				
+				  <table width="100%" cellpadding="2" cellspacing="2" style="border:none;padding:0px;margin:0px;">
+
+<tr>
+'.$icon .'
+<td><h3> '.stripslashes($r[$i]['name']).'</h3></td>
+<td style="text-align:right"> <em>'.date('F jS Y h:i A', strtotime($r[$i]['date'])).'</em></td>
+</tr>
+<tr>
+<td style="text-align:left">'.$project_title .'<br>Notes: <em>'.$r[$i]['notes'].'</em></td>
+
+    
+    <td style="text-align:right;width:170px;">
+	
+	
+	<a href="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/download.php?uid='.$user_ID.'&file='.$r[$i]['file'].'" title="Download" style="margin-right:15px"  ><img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/images/download.png"></a> 
+	<a href="javascript:sp_cu_confirm(\'#sp_cu_confirm_delete\',200,\'?dlg-delete-file='.$r[$i]['id'].'#downloads\');" title="Delete" ><img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/images/delete.png"></a> </td>
+	</tr>
+
+  </tr>
+  </table></div>';	
+					
+			
+}
+
+				
+	
+	return 	$html ;
+	
+}
+
+
+
+
+
+if (!function_exists('display_sp_upload_form')){
+
+function display_sp_upload_form(){
+	
+global $wpdb,$current_user;
+
+
+
+$html .='
+<script type="text/javascript">
+
+
+jQuery(document).ready(function() {
+jQuery("#dlg-upload").bind("click", function() {
+
+jQuery(".sp_change_indicator_button").hide();
+jQuery(".sp_change_indicator").slideDown();
+
+
+
+
+
+
+
+jQuery(\'.sp_change_indicator\').html(\'<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,40,0"  width="204" height="16"  id="mymoviename"><param name="movie" value="'. get_bloginfo("url"). '/wp-content/plugins/sp-client-document-manager/image_138464.swf" /><param name="quality" value="high" /><param name="bgcolor" value="#ffffff" /><embed src="'. get_bloginfo("url"). '/wp-content/plugins/sp-client-document-manager/image_138464.swf" quality="high" bgcolor="#ffffff" width="204" height="16" name="mymoviename" align="" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer"></embed></object><br><em>Please wait, your file is currently uploading! </em>\');
+document.forms["sp_upload_form"].submit();
+return true;
+
+});
+
+
+
+
+});
+
+</script>
+<div style="display:none">
+	<div  id="sp_cu_add_project">
+		<form action="'.$_SERVER['REQUEST_URI'].'" method="post">
+		'.__("Project Name:","sp-cdm").' <input type="text" name="project-name"  style="width:200px !important"> <input type="submit" value="'.__("Add Project","sp-cdm").'" name="add-project">
+	</form>
+	</div>
+<div id="sp_cu_confirm_delete">
+<p>'.get_option('sp_cu_delete').'</p>
+</div>
+
+<div id="sp_cu_thankyou">
+<p>'.get_option('sp_cu_thankyou').'</p>
+</div>
+
+
+		
+
+</div>
+
+
+
+<form  action="'.$_SERVER['REQUEST_URI'].'" method="post" enctype="multipart/form-data" id="upload_form" name="sp_upload_form" >';
+
+
+
+
+			$html .='<div>
+				  <table width="100%" cellpadding="2" cellspacing="2" style="border:none;padding:0px;margin:0px;">
+  <tr>
+    <td width="90">'.__("File Name:","sp-cdm").'</td>
+    <td><input  type="text" name="dlg-upload-name"></td>
+  </tr>
+  
+  ';
+  
+
+  
+ if (CU_PREMIUM == 1){ 
+ $html .= sp_cdm_display_projects(); 
+ $html .= sp_cdm_display_categories(); 
+ }
+ 
+ 
+ 
+  $html .= '
+  <tr>
+    <td>'.__("File:","sp-cdm").'</td>
+    <td>	    <input id="file_upload" name="dlg-upload-file[]" type="file" multiple>
+<div id="upload_list"></div>
+							</td>
+  </tr>
+  <tr>
+    <td>'.__("Notes:","sp-cdm").'</td>
+    <td><textarea style="width:90%;height:70px" name="dlg-upload-notes"></textarea></td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>
+						<div class="sp_change_indicator_button"><input id="dlg-upload" onclick="sp_change_indicator()" type="submit" name="submit" value="Upload" ></div>
+						<div class="sp_change_indicator" ></div>	
+							</td>
+  </tr>
+</table></div>';
+
+
+
+
+$html .='
+
+	</form>
+	
+	
+	';
+	
+	return $html;
+}
+
+function check_folder_sp_client_upload(){
+	global $user_ID;
+	
+	$dir = ''.ABSPATH.'wp-content/uploads/sp-client-document-manager/'.$user_ID.'/';
+	if(!is_dir($dir)){
+	
+		mkdir($dir, 0777);
+	}
+	
+}
+
+function display_sp_client_upload($atts){
+
+	global $wpdb ;
+	global $user_ID;
+	global $current_user;
+     get_currentuserinfo();
+ if ( is_user_logged_in() ) { 
+
+
+
+if($_GET['dlg-delete-file'] != ""){
+	
+	
+	
+		$wpdb->query("
+	DELETE FROM ".$wpdb->prefix."sp_cu WHERE id = ".$_GET['dlg-delete-file']."
+	");
+	
+		 
+}
+
+if($_POST['submit'] != ""){
+
+
+
+	
+	$data = $_POST;
+	$files = $_FILES;
+	$a['uid'] = $user_ID;
+	$a['name'] = addslashes($data['dlg-upload-name']);
+	$a['pid'] = $data['pid'];
+	$a['cid'] = $data['cid'];
+	$a['notes'] = addslashes($data['dlg-upload-notes']);
+	check_folder_sp_client_upload();
+	
+
+	if($files['dlg-upload-file']['name'] != ""){
+	
+	
+	
+	$a['file'] = sp_uploadFile($files);
+    $wpdb->insert(  "".$wpdb->prefix."sp_cu", $a );
+	
+	$to = get_option('admin_email');
+	$headers .= "".__("From:","sp-cdm")." ".$current_user->user_firstname." ".$current_user->user_lastname." <".$current_user->user_email.">\r\n";
+	$message  = "".__("Client uploaded a new document","sp-cdm")."<br><br> ".__("Click here view the files:","sp-cdm")." " . get_bloginfo('wpurl') . "/wp-admin/user-edit.php?user_id=".$user_ID."#downloads";
+	$subject = __("New file upload from client","sp-cdm");
+	wp_mail( $to, $subject, $message, $headers, $attachments );
+	
+		$html .= '<script type="text/javascript">
+
+
+jQuery(document).ready(function() {
+ sp_cu_dialog("#sp_cu_thankyou",400,200);
+});
+</script>';
+}else{
+	
+	$html .= '<p style="color:red">'.__("Please upload a file!","sp-cdm").'</p>';
+	
+}
+}
+
+
+
+
+
+$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where uid = $user_ID  order by date desc", ARRAY_A);
+
+
+
+
+//show uploaded documents
+  $html .= '
+  
+  <script type="text/javascript">
+  jQuery(function() {
+		
+	});
+  </script>
+  
+  
+  
+    <div style="display:none">
+  <div id="cp_cdm_upload_form">
+  '.display_sp_upload_form().'
+  </div>
+  </div>
+  
+
+
+	
+	<div >
+
+  <a href="javascript:sp_cu_dialog(\'#cp_cdm_upload_form\',700,600)"><img src="' . get_bloginfo('url') . '/wp-content/plugins/sp-client-document-manager/images/add.png"> '.__("Add File","sp-cdm").'</a>  
+';
+
+
+
+		$html .=display_sp_thumbnails($r );
+				
+		$html .='</div>';
+
+
+
+
+
+ 
+  } else{
+	  
+	  return '<script type="text/javascript">
+<!--
+window.location = "'. get_bloginfo('url').'/login/?redirect_to='.urlencode($_SERVER['REQUEST_URI']).'"
+//-->
+</script>';
+	 
+  }
+  
+return $html;
+  
+}
+}
+	add_shortcode( 'sp-client-media-manager', 'display_sp_client_upload' );	
+	add_shortcode( 'sp-client-document-manager', 'display_sp_client_upload' );	
+?>
