2e742a46c83cb3887b3c642c7b5b4d4a6b845868
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-01 14:26:37 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.0.9@538531 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax.php
@@ -0,0 +1,282 @@
+<?php
+require( '../../../wp-load.php' );
+	
+	global $wpdb;
+	
+	
+	
+	$function = $_GET['function'];
+	
+	
+	switch($function){
+		
+		case"view-file":
+		if(get_option('sp_cu_wp_folder') == ''){
+	$wp_con_folder = '/';	
+	}else{
+		$wp_con_folder = get_option('sp_cu_wp_folder') ;
+	}
+		
+		$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where id = '".$_GET['id']."'  order by date desc", ARRAY_A);
+		//print_r($r);
+		
+		$ext = substr(strrchr($r[0]['file'], '.'), 1);	
+					
+					if($r[0]['pid'] != 0){
+						  $projecter = $wpdb->get_results("SELECT *
+	
+									 FROM ".$wpdb->prefix."sp_cu_project
+									 WHERE id = '".$r[0]['pid']."'
+									 ", ARRAY_A);
+									 
+									 
+							$project_title ='Project: '.stripslashes($projecter[0]['name']).'';		 	
+					}else{
+						$project_title = ''.__("Project: None","sp-cdm").'';
+					}
+					
+					
+					if($ext== 'png' or $ext == 'jpg' or $ext = 'jpeg' or $ext = 'gif' ){
+					$icon = '<td width="160"><img src="'.$wp_con_folder.'wp-content/uploads/sp-client-document-manager/'.$user_ID.'/'.$r[0]['file'].'" width="150"></td>';	
+					}else{
+					$icon = '';		
+					}
+					
+				$html .= '
+				<div id="view_file_refresh">
+				<div id="sp_cu_viewfile">
+				 <h2> '.stripslashes($r[0]['name']).'</h2>
+				<div class="sp_cu_manage">';
+				 if (CU_PREMIUM == 1){  
+				$html .= sp_cdm_revision_button();
+				 }
+				
+				
+				if(get_option('sp_cu_js_redirect') == 1){
+				$target = 'target="_blank"';	
+				}else{
+				$target = ' ';	
+				}
+				$html .='<a '.$target.' href="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/download.php?fid='.$r[0]['id'].'" title="Download" style="margin-right:15px"  ><img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/images/download.png"> '.__("Download File","sp-cdm").'</a> 
+	<a href="javascript:sp_cu_confirm(\'#sp_cu_confirm_delete\',200,\'?dlg-delete-file='.$r[0]['id'].'#downloads\');" title="Delete" ><img src="' . get_bloginfo('wpurl') . '/wp-content/plugins/sp-client-document-manager/images/delete.png">'.__("Delete File","sp-cdm").'</a>
+	<br> <em>'.date('F jS Y h:i A', strtotime($r[0]['date'])).'</em>
+				</div>
+				
+				<div class="sp_cu_item">
+				
+				
+				  <table width="100%" cellpadding="2" cellspacing="2" style="border:none;padding:0px;margin:0px;">
+
+<tr>
+
+'.$icon .'
+<td>
+<div class="sp_su_project">
+<strong>'.__("Project: ","sp-cdm").' </strong>'.$project_title .'
+</div>
+<div class="sp_su_notes">
+<strong>'.__("Notes: ","sp-cdm").':</strong> <em>'.$r[0]['notes'].'</em>
+</div>';
+
+ if (CU_PREMIUM == 1){ 
+ 
+ 
+$html .='<div class="sp_su_history"><p><strong>'.__("Revision History","sp-cdm").'</strong></p>'.sp_cdm_file_history($r[0]['id']).'</div>';
+ }
+$html .='
+
+
+</td>
+</tr>
+
+  </table></div></div></div>';
+  echo $html;		
+		break;
+		
+		case"file-tree":
+
+		
+	
+
+$_REQUEST['dir'] = urldecode($_REQUEST['dir']);
+$root = ABSPATH;
+
+
+	
+
+
+	if (strpos( $_REQUEST['dir'], 'PID') === false){
+	$r_projects = $wpdb->get_results("SELECT ".$wpdb->prefix."sp_cu.name,".$wpdb->prefix."sp_cu.id,".$wpdb->prefix."sp_cu.pid  ,".$wpdb->prefix."sp_cu.uid,
+											".$wpdb->prefix."sp_cu_project.name AS project_name
+										FROM ".$wpdb->prefix."sp_cu   
+										LEFT JOIN ".$wpdb->prefix."sp_cu_project  ON ".$wpdb->prefix."sp_cu.pid = ".$wpdb->prefix."sp_cu_project.id
+										WHERE ".$wpdb->prefix."sp_cu.uid = '".$_GET['uid']."'  
+										AND pid != 0
+										AND parent = 0 
+										GROUP BY pid
+										ORDER by date desc", ARRAY_A);
+
+										
+	$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where uid = '".$_GET['uid']."'  AND pid = 0 	AND parent = 0  order by date desc", ARRAY_A);
+	
+	}else{
+		
+		$rel_ex = explode("PID", $_REQUEST['dir']); 
+	
+		$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where pid = '".$rel_ex[1]."' AND parent = 0  order by date desc", ARRAY_A);	
+	
+	}
+	
+
+
+	
+	echo "<ul class=\"jqueryFileTree\" style=\"display: none;\">";
+	
+	
+	
+	
+	for($i=0; $i<count($r_projects); $i++){
+		
+		
+		echo "<li class=\"directory collapsed\"><a href=\"#\" rel=\"PID".$r_projects[$i]['pid']."\">" . htmlentities(stripslashes($r_projects[$i]['project_name'])) . "</a></li>";
+			
+	}
+	
+	for($i=0; $i<count( $r ); $i++){
+		
+		$ext = preg_replace('/^.*\./', '', $r[$i]['file']);
+		echo "<li class=\"file ext_$ext\"><a href=\"#\" rel=\"".$r[$i]['id']."\">" . htmlentities(stripslashes($r[$i]['name'])) . "</a></li>";
+		
+	}
+	echo "</ul>";	
+	
+	
+	
+
+
+	
+		break;
+		case "download-project":
+			
+			$user_ID = $_GET['id'];
+			
+				
+		$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where pid = $user_ID  order by date desc", ARRAY_A);		
+		$r_project = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu_project where id = $user_ID  ", ARRAY_A);			
+			$return_file = "".preg_replace('/[^\w\d_ -]/si', '',stripslashes($r_project[0]['name'])).".zip";
+			$zip = new Zip();
+			//@unlink($dir.$return_file);
+				
+				for($i=0; $i<count($r); $i++){
+			
+		$dir = ''.ABSPATH.'wp-content/uploads/sp-client-document-manager/'.$r[$i]['uid'].'/';	
+			$path = '../../../wp-content/uploads/sp-client-document-manager/'.$r[$i]['uid'].'/';	
+					  $zip->addFile(file_get_contents($dir.$r[$i]['file']), $r[$i]['file'] , filectime($dir.$r[$i]['file']));
+					
+				}
+		
+		
+$zip->finalize(); // as we are not using getZipData or getZipFile, we need to call finalize ourselves.
+
+$zip->setZipFile($dir.$return_file);
+		
+
+		
+	header("Location: ".$path.$return_file."");	
+		
+		break;
+		case "download-archive":
+		
+		
+		$user_ID = $_GET['id'];
+			$dir = ''.ABSPATH.'wp-content/uploads/sp-client-document-manager/'.$user_ID.'/';	
+			$path = '../../../wp-content/uploads/sp-client-document-manager/'.$user_ID.'/';			
+			$return_file = "Account.zip";
+				$zip = new Zip();
+				
+		$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu   where uid = $user_ID  order by date desc", ARRAY_A);		
+			
+			//@unlink($dir.$return_file);
+				
+				for($i=0; $i<count($r); $i++){
+			
+		
+					  $zip->addFile(file_get_contents($dir.$r[$i]['file']), $r[$i]['file'] , filectime($dir.$r[$i]['file']));
+					
+				}
+		
+		
+$zip->finalize(); // as we are not using getZipData or getZipFile, we need to call finalize ourselves.
+
+$zip->setZipFile($dir.$return_file);
+		
+
+		
+	header("Location: ".$path.$return_file."");	
+		
+		break;
+	
+	case"email-vendor":
+
+
+	
+	if(count($_POST['vendor_email']) == 0){
+		
+		echo '<p style="color:red;font-weight:bold">'.__("Please select at least one file!","sp-cdm").'</p>';
+	}else{
+	$files = implode(",",$_POST['vendor_email']);
+	$r = $wpdb->get_results("SELECT *  FROM ".$wpdb->prefix."sp_cu  WHERE id IN (".$files.")", ARRAY_A);
+	
+	$message .= '
+	'.$_POST['vendor-message'].'<br><br>';
+	
+	for($i=0; $i<count($r); $i++){
+		
+					
+					if($r[$i]['name'] == ""){
+						
+						$name = $r[$i]['file'];
+					}else{
+						
+						$name = $r[$i]['name'];
+					}
+		
+		
+	$attachment_links .= '<a href="' . get_bloginfo('wpurl') . '/wp-content/uploads/sp-client-document-manager/'.$_POST['user_id'].'/'.$r[$i]['file'].'">'.$name.'</a><br>';
+	$attachment_array[$i] = ''.WP_CONTENT_DIR .'/uploads/sp-client-document-manager/'.$_POST['user_id'].'/'.$r[$i]['file'].'';	
+	}
+	
+	$to = $_POST['vendor'];
+	
+	$headers = array("From: ".get_option('admin_email')."",
+	         "Content-Type: text/html"
+	         );
+	$h = implode("\r\n",$headers) . "\r\n";
+	
+	
+	if($_POST['vendor_attach'] == 3){
+		
+	$attachments =$attachment_array;
+	$message .= $attachment_links;		
+	}elseif($_POST['vendor_attach'] == 1){
+		
+	$attachments =$attachment_array;
+	}else{
+		
+	$message .= $attachment_links;	
+	}
+
+	$subject = ''.__("New files from:","sp-cdm").' '.get_option('blogname').'';
+	wp_mail( $to, $subject, $message, $h, $attachments );
+	
+	
+	
+echo '<p style="color:green;font-weight:bold">'.__("Files Sent to","sp-cdm").' '.$_POST['vendor'].'</p>';
+	}
+	break;	
+		
+		
+		
+	}
+
+?>
