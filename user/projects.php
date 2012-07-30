fd815648a31b30803a3efcf1456bb44fb6a010c3
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-30 14:23:09 +0000

    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.2.3@579212 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ user/projects.php
@@ -0,0 +1,67 @@
+<?php
+
+
+if(!function_exists('sp_cdm_display_projects')){
+function sp_cdm_display_projects(){
+	
+	
+	global $wpdb,$current_user;
+
+if($_POST['add-project'] != ""){
+	
+			$insert['name'] = $_POST['project-name'];
+			$insert['uid'] = $current_user->ID;
+	$wpdb->insert( "".$wpdb->prefix . "sp_cu_project",$insert );
+}
+
+
+if (CU_PREMIUM == 1){  	
+		$find_groups = cdmFindGroups($current_user->ID,'_project');
+			 }
+
+  $projects = $wpdb->get_results("SELECT *
+	
+									 FROM ".$wpdb->prefix."sp_cu_project
+									WHERE  ( uid = '".$current_user->ID."' ".$find_groups.") 
+									 ", ARRAY_A);	
+
+
+  if(count($projects) > 0 or get_option('sp_cu_user_projects') == 1){
+	  $html .= ' <tr>
+    <td>'.__("Project:","sp-cdm").'
+	
+
+	
+	
+	</td>
+    <td>
+	<select name="pid">
+	<option name="" selected="selected">'.__("No Project","sp-cdm").'</option>';
+
+		for($i=0; $i<count($projects); $i++){
+	  $html .= '<option value="'.$projects[$i]['id'].'">'.stripslashes($projects[$i]['name']).'</option>';	
+		}
+	
+	$html .='</select>';
+	
+	if(get_option('sp_cu_user_projects') == 1){
+		
+		$html .= '<a href="javascript:sp_cu_dialog(\'#sp_cu_add_project\',550,130)" class="button" style="margin-left:15px">'.__("Add Project","sp-cdm").'</a>
+		
+	
+		
+		';
+		
+	}
+	$html .='</td>
+  </tr>';
+	  
+  }
+
+	return $html;
+	
+}
+
+}
+
+?>
