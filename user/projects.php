965d442d1a168805ed24b8cb4a53bab1ce5e19c3
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-07 15:37:50 +0000

    = 1.5.8 = * Fixed some bugs * Patched a security whole when accessing a file directly through download.php * Added a new shortcode for premium users * Added a notice for premium users when free version is out of date * Added a notice when a new version of premium is released.
    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.5.8@677531 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ user/projects.php
@@ -0,0 +1,75 @@
+<?php
+
+
+if(!function_exists('sp_cdm_display_projects')){
+function sp_cdm_display_projects(){
+	
+	
+	global $wpdb,$current_user;
+
+if($_POST['add-project'] != ""){
+	
+			$insert['name'] = $_POST['project-name'];
+			$insert['uid'] = $current_user->ID;
+	$wpdb->insert( "".$wpdb->prefix . "sp_cu_project",$insert );
+}
+
+
+if (CU_PREMIUM == 1){  	
+		$find_groups = cdmFindGroups($current_user->ID,'_project');
+			 }
+
+  $projects = $wpdb->get_results("SELECT *
+	
+									 FROM ".$wpdb->prefix."sp_cu_project
+									WHERE  ( uid = '".$current_user->ID."' ".$find_groups.") 
+									 ", ARRAY_A);	
+
+
+  if(count($projects) > 0 or get_option('sp_cu_user_projects') == 1){
+	  $html .= ' <tr>
+    <td><strong>'.__("Project:","sp-cdm").'</strong>
+	
+
+	
+	
+	</td>
+    <td>
+	<select name="pid" class="pid_select">';
+	
+	if(get_option('sp_cu_user_projects_required') == 0){	
+	$html .='<option name="" selected="selected">'.__("No Project","sp-cdm").'</option>';	
+	}
+		for($i=0; $i<count($projects); $i++){
+								
+		if($current_user->last_project == $projects[$i]['id'] ){	
+			$required = ' selected="selected" '	;
+		}else{
+			$required = ''	;
+		}
+	  $html .= '<option value="'.$projects[$i]['id'].'" '.$required.'>'.stripslashes($projects[$i]['name']).'</option>';	
+		}
+	
+	$html .='</select>';
+	
+	if(get_option('sp_cu_user_projects') == 1){
+		
+		$html .= '<a href="javascript:sp_cu_dialog(\'#sp_cu_add_project\',550,130)" class="button" style="margin-left:15px">'.__("Add Project","sp-cdm").'</a>
+		
+	
+		
+		';
+		
+	}
+	$html .='</td>
+  </tr>';
+	  
+  }
+
+	return $html;
+	
+}
+
+}
+
+?>
