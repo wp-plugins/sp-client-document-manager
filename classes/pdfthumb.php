7f591ec1ff449aea353e3bca0866e32804b5b0e5
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-06 17:32:51 +0000

    = 1.6.6 =
    * Fixed an issue with imageMagick
    * New field for ImageMagick that lets you specify a custom location of your imageMagick convert app
    * Sanitize file names to remove special characters and invalid characters from the URL
    * Few minor bug fixes.
    
    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.6.6@708774 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/pdfthumb.php
@@ -0,0 +1,42 @@
+<?php
+require( '../../../../wp-load.php' );
+
+function thumbPdf($pdf, $w,$h)
+{
+    try
+	
+    {
+        $tmp = SP_CDM_UPLOADS_DIR;
+        $format = "png";
+        $source = ABSPATH.$pdf;
+		
+        $dest =  ABSPATH."".$pdf."_small.$format";
+ 		$dest2 =  ABSPATH."".$pdf."_big.$format";
+      
+	   
+            $exec = "convert -scale 80x80 $source $dest";			
+            exec($exec);
+			 $exec2 = "convert -scale 250x250 $source $dest2";			
+            exec($exec2);
+		
+ 
+        $im = new Imagick($dest);
+        header("Content-Type:".$im->getFormat());
+        echo $im;
+    }
+    catch(Exception $e)
+    {
+        echo $e->getMessage();
+    }
+}
+ 
+$file = $_GET['pdf'];
+$w = $_GET['w'];
+$h = $_GET['h'];
+if ($file)
+{
+    thumbPdf($file, $w,$h);
+}
+
+
+?>
