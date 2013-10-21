b7fa3932b0d211ca4ad0aca8e55dd06ec56d83c1
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-21 19:45:10 +0000

    = 1.9.4 =
    * Update to fix issue with batch operations manager
    
    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/1.9.4@791439 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax.php
@@ -0,0 +1,67 @@
+<?php
+
+
+require( '../../../wp-load.php' );
+	
+	
+$upload_dir = wp_upload_dir();	
+	
+	
+	$function = $_GET['function'];
+	
+	
+	switch($function){
+		case "delete-file":
+		echo $spcdm_ajax->delete_file();	
+		break;
+		case "get-file-info":
+		echo $spcdm_ajax->get_file_info();
+		break;
+		case "remove-category":
+		
+		echo $spcdm_ajax->remove_cat();
+	
+		break;	
+		
+		case "save-category":
+		echo $spcdm_ajax->save_cat($_REQUEST['uid']);
+		
+		break;
+		
+		case"view-file":
+		echo $spcdm_ajax->view_file();	
+		break;
+		
+		case "file-list":
+		
+		echo $spcdm_ajax->file_list();		
+		
+		break;
+		
+		case "thumbnails":
+		
+		echo $spcdm_ajax->thumbnails();		
+		
+		break;
+		
+		case "download-project":
+			
+		echo $spcdm_ajax->download_project();		
+		
+		break;
+		case "download-archive":
+		
+			echo $spcdm_ajax->download_archive();		
+		
+		break;
+	
+	case"email-vendor":
+
+		echo $spcdm_ajax->email_vendor();		
+	break;	
+		
+		
+		
+	}
+
+?>
