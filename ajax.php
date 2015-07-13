bbd06784a0f213da4d9a665267d806eec8e0bea0
smartypants <smartypants@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 15:56:33 +0000

    update
    git-svn-id: https://plugins.svn.wordpress.org/sp-client-document-manager/tags/2.5.7.7@1198128 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ajax.php
@@ -0,0 +1,86 @@
+<?php
+
+
+
+$parse_uri = explode( 'wp-content', $_SERVER['SCRIPT_FILENAME'] );
+require_once( $parse_uri[0] . 'wp-load.php' );
+	
+	
+$upload_dir = wp_upload_dir();	
+	
+	
+	$function = $_GET['function'];
+	
+	
+	$spcdm_ajax = new spdm_ajax;
+	
+	
+	
+	switch($function){
+		
+		case "check-file-permissions":
+		
+		echo cdm_file_permissions($_GET['pid']);
+		break;
+		case "check-folder-permissions":
+		
+		echo cdm_folder_permissions($_GET['pid']);
+		break;
+		  case "reload-project-dropdown":
+		echo $spcdm_ajax->project_dropdown();
+		break;
+		case "delete-file":
+		echo $spcdm_ajax->delete_file();	
+		break;
+		case "get-file-info":
+		echo $spcdm_ajax->get_file_info();
+		break;
+		case "remove-category":
+		
+		echo $spcdm_ajax->remove_cat();
+	
+		break;	
+		
+		case "save-category":
+		echo $spcdm_ajax->save_cat($_REQUEST['uid']);
+		
+		break;
+		
+		case"view-file":
+		
+		echo $spcdm_ajax->view_file();	
+		break;
+		
+		case "file-list":
+		
+		echo $spcdm_ajax->file_list();		
+		
+		break;
+		
+		case "thumbnails":
+		
+		echo $spcdm_ajax->thumbnails();		
+		
+		break;
+		
+		case "download-project":
+			
+		echo $spcdm_ajax->download_project();		
+		
+		break;
+		case "download-archive":
+		
+			echo $spcdm_ajax->download_archive();		
+		
+		break;
+	
+	case"email-vendor":
+
+		echo $spcdm_ajax->email_vendor();		
+	break;	
+		
+		
+		
+	
+}
+?>
